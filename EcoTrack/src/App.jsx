39f6ede82a834a7ac8b054af00cafc611295497c
Yash Kotra <yash2019kotra@gmail.com>
2024-01-20 15:21:09 +0530

    Experiment 2: Routing, Protected Routes & Context-Based State Management in EcoTrack

--- EcoTrack/src/App.jsx
+++ EcoTrack/src/App.jsx
@@ -2,17 +2,40 @@ import Header from "./components/header";
 import Logs from "./pages/logs";
 import Dashboard from "./pages/dashboard";
 import { logs } from "./data/logs";
-import { BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import DashboardLayout from "./pages/dashboardLayout";
+import DashboardAnalytics from "./pages/dashboardAnalytic";
+import DashboardSummary from "./pages/dashboardSummary";
 const App = () => {
   return (
     <div>
-      <Header title="EcoTrack - Carbon Footprint Tracker Experiment 1" />
-      <Dashboard />
-      <Logs logs={logs} />
       <BrowserRouter>
-      
-      </BrowserRouter> 
+        <Header title="EcoTrack - Carbon Footprint Tracker Experiment 1" />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <DashboardLayout />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route index element={<DashboardSummary />} />
+          <Route path="analytics" element={<DashboardAnalytics />} />
+          <Route path="summary" element={<DashboardSummary />} />
+          <Route
+            path="/logs"
+            element={
+              <ProtectedRoute>
+                <Logs logs={logs} />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- EcoTrack/src/components/header.jsx
+++ EcoTrack/src/components/header.jsx
@@ -1,9 +1,23 @@
-const header = ({title}) => {
-  return (
-    <header style={{padding:"0.5 rem", backgroundColor:"blue", color:"white", textAlign:"center"}}>
-      <h1>{title}</h1>
-    </header>
-  )
-}
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default header
+const Header = () => {
+  return (
+    <header style={{ padding: "1rem", background: "#220594" }}>
+      <h2>EcoTrack</h2>
+      <nav>
+        <Link to="/" style={{ marginRight: "1rem", color: "white" }}>
+          Dashboard
+        </Link>
+        <Link to="/logs" style={{ marginRight: "1rem", color: "white" }}>
+          Logs
+        </Link>
+        <Link to="/login" style={{ marginRight: "1rem", color: "white" }}>
+          Login
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
