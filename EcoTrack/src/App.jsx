84641abf72c7c1c7d2be0b89f1dd65f1cfd707a5
Yash Kotra <yash2019kotra@gmail.com>
2026-01-19 23:42:13 +0530

    Experiment 2:Routes and AuthContext

--- EcoTrack/src/App.jsx
+++ EcoTrack/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import Logs from "./pages/logs";
 import Dashboard from "./pages/dashboard";
 import { logs } from "./data/logs";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
@@ -9,6 +10,9 @@ const App = () => {
       <Header title="EcoTrack - Carbon Footprint Tracker Experiment 1" />
       <Dashboard />
       <Logs logs={logs} />
+      <BrowserRouter>
+      
+      </BrowserRouter> 
     </div>
   );
 };
